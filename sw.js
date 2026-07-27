2dc57b9cd4e49881ad615d58cb5ef973a6dd45e6
Alireza <alireza@footballma.ir>
2026-07-27 13:24:26 +0000

    Deploy: latest with PWA fix

--- sw.js
+++ sw.js
@@ -1,17 +1,22 @@
-const CACHE = 'footballmedia-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'fm-v0.0.1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
     e.respondWith(
-        fetch(e.request).then(r => {
-            const clone = r.clone();
+        caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+            const clone = resp.clone();
             caches.open(CACHE).then(c => c.put(e.request, clone));
-            return r;
-        }).catch(() => caches.match(e.request))
+            return resp;
+        }))
     );
 });
